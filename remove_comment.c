cd51f048a010bf7f69f7db404f49db60cbc4814d
leumars-o <samex150@gmail.com>
2023-11-11 00:03:28 +0100

    program function that ignores comments in command passed

---
+++ remove_comment.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+void removeComment(char *cmd)
+{
+	size_t len, i;
+
+	if (cmd != NULL)
+	{
+		len = _strlen(cmd);
+		cmd[len - 1] = '\0';
+
+		for (i = 0; cmd[i] != '\0'; i++)
+		{
+			if (cmd[i] == '#')
+			{
+				cmd[i] = '\0';
+				break;
+			}
+		}
+	}
+	else
+		perror("ERROR: CMD NULL");
+}
